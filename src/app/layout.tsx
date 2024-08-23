f55a581bffcbe14e959800bd043cc65cf8f6a818
pinarsuvacoglu <pinarsuvac@gmail.com>
2024-08-23 20:15:19 +0300

    add logo :)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/Bunsy-weather-logo.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
